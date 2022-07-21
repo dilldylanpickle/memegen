4b9819b3f32c6c22fadc6aa3b6a8ace96d42c596
DeepVoicePickle <62671152+dilldylanpickle@users.noreply.github.com>
2022-07-21 00:23:14 -0500

    Add files via upload

---
+++ src/application/controller/MainController.java
@@ -0,0 +1,20 @@
+package application.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainController {
+	public class PleaseProvideControllerClassName {
+
+	    @FXML
+	    private Button buttonUploadImageFile;
+
+	    @FXML
+	    void hand(ActionEvent event) {
+
+	    }
+
+	}
+
+
+}
